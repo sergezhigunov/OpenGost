41e75da4b6f540811db69a93c7f7924c39503e0f
Serge Zhigunov <serge.zhigunov@outlook.com>
2016-03-30 20:53:25 +0500

    Initial commit

---
+++ src/Gost.Security.Cryptography/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("668d7f7f-ce7e-45fb-8cf2-a8ed35d72f3f")]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en-US")]
---
+++ test/Gost.Security.Cryptography.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("49680671-0348-43b3-8684-fc744c738f0c")]

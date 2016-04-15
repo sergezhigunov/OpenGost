866b3fa11cd40edcaa354443236ce2792fc6a988
Serge Zhigunov <serge.zhigunov@outlook.com>
2016-04-15 23:02:18 +0500

    Add Streebog HMAC test cases

--- test/Gost.Security.Cryptography.Tests/Security/Cryptography/HmacStreebog256Tests.cs
+++ test/Gost.Security.Cryptography.Tests/Security/Cryptography/HmacStreebog256Tests.cs
@@ -13,7 +13,10 @@ namespace Gost.Security.Cryptography
         protected override HashAlgorithm CreateHashAlgorithm()
             => Streebog256.Create();
 
-        [Theory(DisplayName = nameof(Streebog256) + "_" + nameof(ComputeHmac), Skip = "No test cases avaliable yet")]
+        [Theory(DisplayName = nameof(Streebog256) + "_" + nameof(ComputeHmac))]
+        [InlineData("0126bdb87800af214341456563780100",
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+            "a1aa5f7de402d7b3d323f2991c8d4534013137010a83754fd0af6d7cd4922ed9")]
         public void ComputeHmac(string dataHex, string keyHex, string expectedHmacHex)
             => VerifyHmac(dataHex, keyHex, expectedHmacHex);
 

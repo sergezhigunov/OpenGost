3939a0264e87468cbc2e13585b3316b76cf0f090
Serge Zhigunov <serge.zhigunov@outlook.com>
2016-06-21 03:49:52 +0500

    Add GostECDsa signature formatter and deformatter

---
+++ src/Gost.Security.Cryptography/Security/Cryptography/GostECDsaSignatureDeformatter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Gost.Security.Cryptography
+{
+    /// <summary>
+    /// Verifies a <see cref="GostECDsa"/> PKCS#1 v1.5 signature.
+    /// </summary>
+    public class GostECDsaSignatureDeformatter : AsymmetricSignatureDeformatter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GostECDsaSignatureDeformatter"/> class.
+        /// </summary>
+        public GostECDsaSignatureDeformatter()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GostECDsaSignatureDeformatter"/> class with the specified key.
+        /// </summary>
+        /// <param name="key">
+        /// The instance of <see cref="GostECDsa"/> that holds the key.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="key"/> is <c>null</c>.
+        /// </exception>
+        public GostECDsaSignatureDeformatter(AsymmetricAlgorithm key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Specifies the hash algorithm for the <see cref="GostECDsa"/> signature deformatter.
+        /// </summary>
+        /// <param name="strName">
+        /// The name of the hash algorithm to use for the signature deformatter. 
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="strName"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="CryptographicUnexpectedOperationException">
+        /// The <paramref name="strName"/> parameter does not map to the hash algorithm.
+        /// </exception>
+        public override void SetHashAlgorithm(string strName)
+        {
+            if (strName == null) throw new ArgumentNullException(nameof(strName));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Specifies the key to be used for the <see cref="GostECDsa"/> signature deformatter.
+        /// </summary>
+        /// <param name="key">
+        /// The instance of <see cref="GostECDsa"/> that holds the key. 
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="key"/> is <c>null</c>.
+        /// </exception>
+        public override void SetKey(AsymmetricAlgorithm key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Verifies the <see cref="GostECDsa"/> signature on the data.
+        /// </summary>
+        /// <param name="rgbHash">
+        /// The data signed with <paramref name="rgbSignature"/>.
+        /// </param>
+        /// <param name="rgbSignature">
+        /// The signature to be verified for <paramref name="rgbHash"/>. 
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the signature is valid for the data;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="rgbHash"/> is <c>null</c>.
+        /// -or-
+        /// <paramref name="rgbSignature"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="CryptographicUnexpectedOperationException">
+        /// The <see cref="GostECDsa"/> key is missing.
+        /// </exception>
+        public override bool VerifySignature(byte[] rgbHash, byte[] rgbSignature)
+        {
+            if (rgbHash == null) throw new ArgumentNullException(nameof(rgbHash));
+            if (rgbSignature == null) throw new ArgumentNullException(nameof(rgbSignature));
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Gost.Security.Cryptography/Security/Cryptography/GostECDsaSignatureFormatter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Gost.Security.Cryptography
+{
+    /// <summary>
+    /// Creates a <see cref="GostECDsa"/> signature.
+    /// </summary>
+    public class GostECDsaSignatureFormatter : AsymmetricSignatureFormatter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GostECDsaSignatureFormatter"/> class.
+        /// </summary>
+        public GostECDsaSignatureFormatter()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GostECDsaSignatureFormatter"/> class with the specified key.
+        /// </summary>
+        /// <param name="key">
+        /// The instance of <see cref="GostECDsa"/> that holds the key.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="key"/> is <c>null</c>.
+        /// </exception>
+        public GostECDsaSignatureFormatter(AsymmetricAlgorithm key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates the <see cref="GostECDsa"/> PKCS #1 signature for the specified data.
+        /// </summary>
+        /// <param name="rgbHash">
+        /// The data to be signed.
+        /// </param>
+        /// <returns>
+        /// The digital signature for the specified data.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="rgbHash"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="CryptographicUnexpectedOperationException">
+        /// The OID is <c>null</c>.
+        /// -or-
+        /// The <see cref="GostECDsa"/> key is <c>null</c>.
+        /// </exception>
+        public override byte[] CreateSignature(byte[] rgbHash)
+        {
+            if (rgbHash == null) throw new ArgumentNullException(nameof(rgbHash));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Specifies the hash algorithm for the <see cref="GostECDsa"/> signature formatter.
+        /// </summary>
+        /// <param name="strName">
+        /// The name of the hash algorithm to use for the signature formatter. 
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="strName"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="CryptographicUnexpectedOperationException">
+        /// The <paramref name="strName"/> parameter does not map to the hash algorithm.
+        /// </exception>
+        public override void SetHashAlgorithm(string strName)
+        {
+            if (strName == null) throw new ArgumentNullException(nameof(strName));
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Specifies the key to be used for the <see cref="GostECDsa"/> signature formatter.
+        /// </summary>
+        /// <param name="key">
+        /// The instance of <see cref="GostECDsa"/> that holds the key. 
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="key"/> is <c>null</c>.
+        /// </exception>
+        public override void SetKey(AsymmetricAlgorithm key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            throw new NotImplementedException();
+        }
+    }
+}

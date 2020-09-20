310456cb54cabfff741f7bde5ed9170c37f8e8e6
Sergey Zhigunov <serge.zhigunov@outlook.com>
2020-09-20 22:35:00 +0500

    #4 Add benchmark project

---
+++ benchmarks/OpenGost.Security.Cryptography.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace OpenGost.Security.Cryptography.Benchmarks
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+            => BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+    }
+}

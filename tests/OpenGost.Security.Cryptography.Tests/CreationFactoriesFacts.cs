52724375e23f238d78cab3f3ff334fd020858693
Sergey Zhigunov <serge.zhigunov@mail.ru>
2022-11-18 02:52:26 +0500

    Fix a few obsolete creation factory usage warnings

--- tests/OpenGost.Security.Cryptography.Tests/CreationFactoriesFacts.cs
+++ tests/OpenGost.Security.Cryptography.Tests/CreationFactoriesFacts.cs
@@ -49,38 +49,32 @@ public class CreationFactoriesFacts
 
         return new[]
         {
-
-            TestCase(typeof(GostECDsa), AsymmetricAlgorithm.Create),
-            TestCase(typeof(GostECDsa), ECDsa.Create),
             TestCase(typeof(GostECDsa), GostECDsa.Create),
-
-            TestCase(typeof(Grasshopper), SymmetricAlgorithm.Create),
             TestCase(typeof(Grasshopper), Grasshopper.Create),
-
-            TestCase(typeof(Magma), SymmetricAlgorithm.Create),
             TestCase(typeof(Magma), Magma.Create),
-
-            TestCase(typeof(Streebog256), HashAlgorithm.Create),
             TestCase(typeof(Streebog256), Streebog256.Create),
-
-            TestCase(typeof(Streebog512), HashAlgorithm.Create),
             TestCase(typeof(Streebog512), Streebog512.Create),
-
-            TestCase(typeof(CMACGrasshopper), HashAlgorithm.Create),
-            TestCase(typeof(CMACGrasshopper), KeyedHashAlgorithm.Create),
             TestCase(typeof(CMACGrasshopper), CMAC.Create),
-
-            TestCase(typeof(CMACMagma), HashAlgorithm.Create),
-            TestCase(typeof(CMACMagma), KeyedHashAlgorithm.Create),
             TestCase(typeof(CMACMagma), CMAC.Create),
 
+#pragma warning disable SYSLIB0045
+            TestCase(typeof(GostECDsa), ECDsa.Create),
+            TestCase(typeof(GostECDsa), AsymmetricAlgorithm.Create),
+            TestCase(typeof(Grasshopper), SymmetricAlgorithm.Create),
+            TestCase(typeof(Magma), SymmetricAlgorithm.Create),
+            TestCase(typeof(Streebog256), HashAlgorithm.Create),
+            TestCase(typeof(Streebog512), HashAlgorithm.Create),
+            TestCase(typeof(CMACGrasshopper), HashAlgorithm.Create),
+            TestCase(typeof(CMACGrasshopper), KeyedHashAlgorithm.Create),
+            TestCase(typeof(CMACMagma), HashAlgorithm.Create),
+            TestCase(typeof(CMACMagma), KeyedHashAlgorithm.Create),
             TestCase(typeof(HMACStreebog256), HashAlgorithm.Create),
             TestCase(typeof(HMACStreebog256), KeyedHashAlgorithm.Create),
             TestCase(typeof(HMACStreebog256), HMAC.Create),
-
             TestCase(typeof(HMACStreebog512), HashAlgorithm.Create),
             TestCase(typeof(HMACStreebog512), KeyedHashAlgorithm.Create),
             TestCase(typeof(HMACStreebog512), HMAC.Create),
+#pragma warning restore SYSLIB0045
         };
     }
 

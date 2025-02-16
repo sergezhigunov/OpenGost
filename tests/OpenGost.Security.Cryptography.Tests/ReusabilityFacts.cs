ec0e59d5f853e34e1a088e81548e11ba3135c391
Sergey Zhigunov <serge.zhigunov@mail.ru>
2025-02-16 20:15:37 +0500

    Add a few missing ReusabilityFacts cases

--- tests/OpenGost.Security.Cryptography.Tests/ReusabilityFacts.cs
+++ tests/OpenGost.Security.Cryptography.Tests/ReusabilityFacts.cs
@@ -3,24 +3,21 @@
 public class ReusabilityFacts
 {
     [Theory]
-    [MemberData(nameof(ReusabilityHashAlgorithms))]
-    public void ReuseHashAlgorithm(Func<HashAlgorithm> hashAlgorithmFactory)
+    [InlineData(typeof(CMACGrasshopper))]
+    [InlineData(typeof(CMACMagma))]
+    [InlineData(typeof(HMACStreebog256))]
+    [InlineData(typeof(HMACStreebog512))]
+    [InlineData(typeof(Streebog256Managed))]
+    [InlineData(typeof(Streebog512Managed))]
+    public void ReuseHashAlgorithm(Type hashAlgorithmType)
     {
-        using var hashAlgorithm = hashAlgorithmFactory.Invoke();
+        using var hashAlgorithm = (HashAlgorithm)Activator.CreateInstance(hashAlgorithmType)!;
         byte[] input = [0x08, 0x06, 0x07, 0x05, 0x03, 0x00, 0x09];
 
         var hash1 = hashAlgorithm.ComputeHash(input);
         var hash2 = hashAlgorithm.ComputeHash(input);
 
         Assert.Equal(hash1, hash2);
+        Assert.NotSame(hash1, hash2);
     }
-
-    public static TheoryData<Func<HashAlgorithm>> ReusabilityHashAlgorithms()
-        => new()
-        {
-            { () => new Streebog256Managed() },
-            { () => new Streebog512Managed() },
-            { () => new CMACGrasshopper() },
-            { () => new CMACMagma() },
-        };
 }
